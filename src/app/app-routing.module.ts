01a1fa2ce8f3d944a3d172965d48bb8cf2a83cd5
timothybaraka <timothybaraka44@gmail.com>
2020-01-20 10:34:37 +0300

    Add routing to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { UserComponent } from '../app/user/user.component';
 import { RepoDisplayComponent } from '../app/repo-display/repo-display.component'
 
 const routes: Routes = [
-    // {path: 'users',component: UserComponent},
-    // {path: 'repositories',component: RepoDisplayComponent }
+    {path: 'users',component: UserComponent},
+    {path: 'repositories',component: RepoDisplayComponent }
 ];
 
 @NgModule({
